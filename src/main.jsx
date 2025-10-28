4d31e63a97f93de6b59df552d934f02d4699fe81
nada-mohamed-3 <nadamoamed448@gmail.com>
2025-10-28 21:11:22 +0300

    two-commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,6 @@ import "react-toastify/dist/ReactToastify.css";
 import { Scroll } from "lucide-react";
 import ScrollToTop from "react-scroll-to-top";
 import LocationProvider from "./Context/LocationContext.jsx";
-;
-
-
-
-
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -25,22 +20,31 @@ createRoot(document.getElementById("root")).render(
   <DataProvider>
     <CartProvider>
       <LocationProvider>
-            <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-        <ScrollToTop color="white" smooth style={{ backgroundColor:'#fa2d37', display: "flex", alignItems: "center", justifyContent: "center"}}/>
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-             </ClerkProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+          <ScrollToTop
+            color="white"
+            smooth
+            style={{
+              backgroundColor: "#fa2d37",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </ClerkProvider>
       </LocationProvider>
     </CartProvider>
   </DataProvider>
